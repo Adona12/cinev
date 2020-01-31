e033218a4896723d4e6f007a0487c4f981a15b20
hannasamuel20 <hanna17st@gmail.com>
2020-01-31 15:07:51 +0300

    model

--- Cinev/Cinev/Views/UpcomingDetails.xaml.cs
+++ Cinev/Cinev/Views/UpcomingDetails.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Cinev.Models;
 
 namespace Cinev.Views
 {
@@ -17,20 +19,36 @@ namespace Cinev.Views
 
 
             InitializeComponent();
-            Title.Text = upcoming.Title;
 
-         MovieImage.Source = new UriImageSource()
+            using (var webClient = new WebClient())
+
             {
-
-                Uri = new Uri(upcoming.FullBackPath)
-
-            };
-            OverView.Text = upcoming.Overview;
+                string s = "https://api.themoviedb.org/3/movie/429617?api_key=f4b8e415cb9ab402e5c1d72176cab35b";
+                string mid = Convert.ToString(upcoming.Id);
+                string jsonString = webClient.DownloadString("https://api.themoviedb.org/3/movie/" + mid + "?api_key=f4b8e415cb9ab402e5c1d72176cab35b");
 
 
+                var r = MUpcomingDetails.FromJson(jsonString);
 
+                Title.Text = upcoming.Title;
+
+                MovieImage.Source = new UriImageSource()
+                {
+
+                    Uri = new Uri(upcoming.FullBackPath)
+
+                };
+                OverView.Text = upcoming.Overview;
+
+
+
+            }
+
+      
 
         }
+
+    
 
         private void AddToWish_Clicked(object sender, EventArgs e)
         {
