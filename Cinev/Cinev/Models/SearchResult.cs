64eff346c4097a58228ce7dd0ec9d9c8e6830dc5
hannasamuel20 <hanna17st@gmail.com>
2020-02-01 03:15:21 +0300

    search

---
+++ Cinev/Cinev/Models/SearchResult.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Cinev.Model
+{
+
+    public partial class SearchResult
+    {
+        [JsonProperty("results")]
+        public List<SearchResult> Results { get; set; }
+
+
+
+
+    }
+
+
+    public partial class SearchResult
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        public static SearchResult FromJson(string json) => JsonConvert.DeserializeObject<SearchResult>(json, Cinev.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this SearchResult self) => JsonConvert.SerializeObject(self, Cinev.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
